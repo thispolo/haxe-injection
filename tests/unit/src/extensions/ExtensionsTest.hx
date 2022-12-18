890a7ffc507706f120ee4f5b89bb5061cb2397d1
paul_sg_cross <paul.sg.cross@gmail.com>
2022-12-18 14:35:42 +0000

    Update test

--- tests/unit/src/extensions/ExtensionsTest.hx
+++ tests/unit/src/extensions/ExtensionsTest.hx
@@ -3,74 +3,25 @@ package extensions;
 import utest.Assert;
 import hx.injection.Service;
 import hx.injection.ServiceCollection;
-import hx.injection.ServiceType;
-
-// comment this when using version 2.1.0
 using hx.injection.ServiceExtensions;
-// ------------------------------------------------------------
 
-enum abstract ServiceStatus(Int)
+enum abstract Response(Int)
 {
 	public var Ok = 123;
 }
 
-enum abstract ServiceBaseErrCodes(Int)
+abstract class AbstractBase implements Service
 {
-	public var OK = 0;
+	public abstract function Start():Response;
 }
 
-interface IService extends Service
-{
-	public function Start():ServiceStatus;
-	public function Pause():ServiceStatus;
-	public function Stop():ServiceStatus;
-}
-
-interface IServiceBase<TServiceErrCodeEnum> extends IService
-{
-	public var TargetState(default, null):ServiceStatus;
-	public var CurrentState(default, null):ServiceStatus;
-	public var ErrCode(default, null):TServiceErrCodeEnum;
-}
-
-interface INodeBase<TServiceErrCodeEnum> extends IServiceBase<TServiceErrCodeEnum> {}
-
-abstract class NodeBase<ServiceErrCodeEnum> implements INodeBase<ServiceErrCodeEnum>
-{
-	public var Status(default, null):ServiceStatus;
-	public var ErrCode(default, null):ServiceErrCodeEnum;
-
-	public abstract function Start():ServiceStatus;
-
-	public abstract function Pause():ServiceStatus;
-
-	public abstract function Stop():ServiceStatus;
-
-	public abstract function SendMessage(message:String):Bool;
-}
-
-class Node extends NodeBase<ServiceBaseErrCodes>
+class SomeClass extends AbstractBase
 {
 	public function new() {}
 
-	public function Start():ServiceStatus
+	public function Start():Response
 	{
-		return ServiceStatus.Ok;
-	}
-
-	public function Pause():ServiceStatus
-	{
-		return ServiceStatus.Ok;
-	}
-
-	public function Stop():ServiceStatus
-	{
-		return ServiceStatus.Ok;
-	}
-
-	public function SendMessage(message:String):Bool
-	{
-		return true;
+		return Response.Ok;
 	}
 }
 
@@ -79,7 +30,7 @@ final class ExtensionsTest extends utest.Test
 	public function testMain()
 	{
 		var collection:ServiceCollection = new ServiceCollection();
-		collection.addSingleton(IService, Node);
-		Assert.equals(ServiceStatus.Ok, collection.createProvider().getService(IService).Start());
+		collection.addSingleton(AbstractBase, SomeClass);
+		Assert.equals(Response.Ok, collection.createProvider().getService(AbstractBase).Start());
 	}
 }
